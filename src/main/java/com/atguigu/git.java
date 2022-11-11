7e6f62561b82919ed4fc5670b3edfeea251fcb7b
JiaHong <jiahongyaonuli@qq.com>
2022-11-11 18:48:58 +0800

    first commit

---
+++ src/main/java/com/atguigu/git.java
@@ -0,0 +1,16 @@
+package com.atguigu;
+
+/***
+ * @title git
+ * @author JiaHong
+ * @version 1.0.0
+ * @date 2022/11/11 18:40
+ **/
+
+public class git {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+
+    }
+}
